4755ca2f6930264f973d1f34dc161a7ea8d75bc5
zartasha-khan123 <zartashimran12345@gmail.com>
2025-01-03 01:24:59 +0500

    up2

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -9,7 +9,7 @@ export const client = createClient({
   dataset, // Replace with your dataset name
   apiVersion, // Replace with your API version
   useCdn: false, // Set to true for faster read-only queries
-  token: process.env.SANITY_API_TOKEN, // Ensure this token has "Write" permissions
+  token: process.env.NEXT_PUBLIC_SANITY_API_TOKEN, // Ensure this token has "Write" permissions
 });
 
 
